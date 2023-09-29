8d2724f0cb5088e72f0c5d09568eb572af80501f
offdopedro1303 <130668467+offdopedro1303@users.noreply.github.com>
2023-09-29 11:12:39 -0300

    Create exercicio4.php

---
+++ exercicio4.php
@@ -0,0 +1,10 @@
+<?php
+$numero1 = 70;
+
+if ($numero1 > 50) {
+    echo "Você receberá uma multa.";} 
+    elseif ($numero1 < 50) {
+    echo "Você está na velocidade correta.";}
+    elseif ($numero1 >= 40 && $numero1 < 50) {
+    echo "Atenção com sua velocidade";}
+?>
