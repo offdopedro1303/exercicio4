761da854351f4d69377b04d5afea6b250c664ff2
offdopedro1303 <130668467+offdopedro1303@users.noreply.github.com>
2023-09-29 11:56:17 -0300

    commit do pedro

--- exercicio4.php
+++ exercicio4.php
@@ -1,10 +1,10 @@
 <?php
 $numero1 = 30;
 
-if ($numero1 > 50) {
+if ($numero1 >= 51) {
     echo "Você receberá uma multa.";} 
     elseif ($numero1 >= 41 && $numero1 < 50) {
-    echo "Atenção com sua velocidade";}
+    echo "A velocidade está correta";}
     elseif ($numero1 = 40) { 
     echo "Atenção com a velocidade";}
 ?>
